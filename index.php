176e5029db3473ff4cefd0eccc48324c884c09c5
pranshu2019 <54625716+pranshu2019@users.noreply.github.com>
2019-11-30 16:38:48 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,750 @@
+﻿<!DOCTYPE html>
+
+
+<head>
+	
+<meta charset="utf-8">
+	
+<meta name="viewport" content="width=devidev-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Online Ticket Reservation</title>
+	
+	
+<!-- [ FONT-AWESOME ICON ] 
+        
+=========================================================================================================================-->
+	
+<link rel="stylesheet" type="text/css" href="library/font-awesome-4.3.0/css/font-awesome.min.css">
+
+	
+<!-- [ PLUGIN STYLESHEET ]
+        
+=========================================================================================================================-->
+	
+<link rel="shortcut icon" type="image/x-icon" href="images/icon.png">
+	
+<link rel="stylesheet" type="text/css" href="css/animate.css">
+	
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+        
+<link rel="stylesheet" type="text/css" href="css/owl.theme.css">
+	
+<link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+	
+<!-- [ Boot STYLESHEET ]
+        
+=========================================================================================================================-->
+	
+<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap-theme.min.css">
+	
+<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap.css">
+       
+<!-- [ DEFAULT STYLESHEET ] 
+        
+=========================================================================================================================-->
+	
+<link rel="stylesheet" type="text/css" href="css/style.css">
+        
+<link rel="stylesheet" type="text/css" href="css/responsive.css">
+	
+<link rel="stylesheet" type="text/css" href="css/color/rose.css">
+        
+
+</head>
+<body >
+
+<!-- [ LOADERs ]
+
+================================================================================================================================-->
+	
+    <div class="preloader">
+    
+<div class="loader theme_background_color">
+        
+<span></span>
+      
+    
+</div>
+</div>
+<!-- [ /PRELOADER ]
+
+=============================================================================================================================-->
+
+<!-- [WRAPPER ]
+
+=============================================================================================================================-->
+
+<div class="wrapper">
+  
+<!-- [NAV]
+ 
+============================================================================================================================-->    
+   
+<!-- Navigation
+    ==========================================-->
+    
+<nav  class=" nim-menu navbar navbar-default navbar-fixed-top">
+      
+<div class="container">
+        
+<!-- Brand and toggle get grouped for better mobile display -->
+        
+<div class="navbar-header">
+          
+<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            
+<span class="sr-only">Toggle navigation</span>
+            
+<span class="icon-bar"></span>
+            
+<span class="icon-bar"></span>
+            
+<span class="icon-bar"></span>
+          
+</button>
+            
+<a class="navbar-brand" href="index.html">Online<span class="themecolor"> T</span>ickets</a>
+        
+</div>
+
+        
+<!-- Collect the nav links, forms, and other content for toggling -->
+        
+<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          
+<ul class="nav navbar-nav navbar-right">
+            
+<li><a href="#home" class="page-scroll"><h3>Home</h3></a></li>
+            
+<li><a href="#one" class="page-scroll"><h3>About</h3></a></li>
+                                   
+<li><a href="#four" class="page-scroll"><h3>Inspire</h3></a></li>
+                                 
+<li><a href="#seven" class="page-scroll"><h3>Featues</h3></a></li>
+            
+<li><a href="#eight" class="page-scroll"><h3>Contact</h3></a></li>
+
+<li><a href="admin/" class="page-scroll"><h3>Login</h3></a></li>
+          
+</ul>
+        
+</div>
+<!-- /.navbar-collapse -->
+      
+</div><!-- /.container-fluid -->
+    
+</nav>
+
+
+   
+<!-- [/NAV]
+ 
+============================================================================================================================--> 
+    
+   
+<!-- [/MAIN-HEADING]
+ 
+============================================================================================================================--> 
+   
+<section class="main-heading" id="home">
+       
+<div class="overlay">
+           
+<div class="container">
+               
+<div class="row">
+                   
+<div class="main-heading-content col-md-12 col-sm-12 text-center">
+        
+<h1 class="main-heading-title"><span class="main-element themecolor" data-elements=" Online Ticket, Online Ticket, Online Ticket"></span></h1>
+ 
+<h1 class="main-heading-title"><span class="main-element themecolor" data-elements=" Reservation System, Reservation System, Reservation System"></span></h1>
+       
+<p class="main-heading-text">WELCOME TO,<br/>ONLINE TICKET RESERVATION SYSTEM</p>
+        
+<div class="btn-bar">
+          
+<a href="reserved.php" class="btn btn-custom theme_background_color">Reserve Now</a>
+                 
+</div>
+      
+</div>
+               
+</div>
+           
+</div>
+       
+</div>  
+      
+   
+</section>
+ 
+<section class="main-heading" id="home">
+       
+<div class="overlay">
+           
+<div class="container">
+               
+<div class="row">
+                   
+<div class="main-heading-content col-md-12 col-sm-12 text-center">
+        
+<h1 class="main-heading-title">Plan your Journey</h1>
+        
+<p class="main-heading-text">
+<br/></p>
+        
+<div class="btn-bar">
+          
+<a href="#" class="btn btn-custom theme_background_color">Ge Started</a>
+          
+<a href="#four" class="btn btn-custom-outline">Contact Us</a>
+        
+</div>
+      
+</div>
+               
+</div>
+           
+</div>
+       
+</div>  
+      
+   
+</section>
+    
+ 
+<!-- [/MAIN-HEADING]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+<!-- [ABOUT US]
+ 
+============================================================================================================================-->
+ 
+<section class="aboutus white-background black" id="one">
+     
+<div class="container">
+         
+<div class="row">
+             
+<div class="col-md-12 text-center black">
+                 
+<h3 class="title">ABOUT <span class="themecolor">US</span></h3>
+            
+<p class="a-slog"></p> 
+             
+</div>
+         
+</div>
+         
+<div class="gap">
+             
+         
+</div>
+         
+         
+<div class="row about-box">
+          
+<div class="col-sm-4 text-center">
+            
+<div class="margin-bottom">
+              
+<i class="fa fa-newspaper-o"></i>
+              
+<h4>POWER OF FLEXIBILITY</h4>
+              
+<p class="black"></p>
+            
+</div> <!-- / margin -->
+          
+</div> <!-- /col -->
+          
+<div class="col-sm-4 about-line text-center">
+            
+<div class="margin-bottom">
+              
+<i class="fa fa-diamond"></i>
+              
+<h4>FULLY RESPONSIVE</h4>
+              
+<p class="black"></p>
+            
+</div> <!-- / margin -->
+          
+</div><!-- /col -->
+          
+<div class="col-sm-4 text-center">
+            
+<div class="margin-bottom">
+              
+<i class="fa fa-area-chart"></i>
+              
+<h4>GREAT WEB IDEAS</h4>
+              
+<p class="black"></p>
+            
+</div> <!-- / margin -->
+          
+</div><!-- /col -->
+        
+</div> <!-- /row -->
+         
+         
+         
+         
+     
+</div>
+ </section>
+ 
+ 
+ 
+<!-- [/ABOUTUS]
+ 
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+ 
+ 
+<!-- [Inspire]
+ 
+============================================================================================================================-->
+ 
+<section class="Inspire" id="four">
+     
+<div class="overlay">
+         
+<div class="container">
+             
+<div class="row">
+                
+ <article class="col-md-12 text-center">
+           
+ <div class="intermediate-container">
+             
+ <div class="subheading">
+                 
+ <h4>Are You Ready To <span class="themecolor">Enjoy?</span></h4>
+           
+   </div>
+             
+ <div class="heading">
+              
+  <h2>inspire your customer here!</h2>
+          
+    </div>
+              
+<div class="">
+              
+  <a class="btn btn-custom-outline" href="#"><span>get started</span></a>
+        
+      </div>
+        
+    </div>
+       
+   </article>
+       
+      </div>
+    
+    </div>
+    
+ </div>
+ 
+</section>
+ 
+ 
+<!-- [/Inspire]
+ 
+
+============================================================================================================================-->
+ 
+ 
+<!-- [/Features]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+<section class="Features white-background black" id="seven">
+     
+ <div class="container" style="height:400px">
+        
+<div class="row text-center">
+          
+<div class="col-md-12">
+              
+<h3 class="title"><span class="themecolor" style="margin-top:20px">What makes us best</span></h3>
+            
+<p class="a-slog"></p>
+          
+</div> <!-- /col -->
+        
+</div> <!-- /row -->
+        
+<div class="gap"></div>
+
+        
+<div class="row">
+          
+<div class="col-sm-4">
+            
+<div class="nim-service margin-bottom">
+              
+<i class="fa fa-diamond"></i>
+              
+<div class="nim-service-detail">
+                
+<h4>Booking made Easy</h4>
+                
+<p></p>
+   
+           </div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+          
+</div> <!-- /col -->
+
+          
+<div class="col-sm-4">
+            
+<div class="nim-service margin-bottom">
+             
+ <i class="fa fa-tablet"></i>
+             
+ <div class="nim-service-detail">
+              
+  <h4>Faster Booking</h4>
+              
+  <p></p>
+ 
+             </div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+          
+</div> <!-- /col -->
+
+          
+<div class="col-sm-4">
+            
+<div class="nim-service margin-bottom">
+              
+<i class="fa fa-magic"></i>
+              
+<div class="nim-service-detail">
+                
+<h4>Secure Payment</h4>
+                
+<p></p>
+ 
+             </div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+         
+ </div> <!-- /col -->       
+        
+</div> <!-- end row -->
+
+        
+<div class="row">
+          
+<div class="col-sm-4">
+            
+<div class="nim-service margin-bottom">
+             
+ <i class="fa fa-rocket"></i>
+             
+ <div class="nim-service-detail">
+               
+ <h4>User-Friendly Interface</h4>
+                
+<p></p>
+          
+    </div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+         
+ </div> <!-- /col -->
+
+          
+<div class="col-sm-4">
+           
+ <div class="nim-service margin-bottom">
+              
+<i class="fa fa-map-marker"></i>
+              
+<div class="nim-service-detail">
+                
+<h4>Fast & Secure Database</h4>
+                
+<p></p>
+             
+ </div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+          
+</div> <!-- /col -->
+
+          
+<div class="col-sm-4">
+            
+<div class="nim-service margin-bottom">
+             
+ <i class="fa fa-paypal"></i>
+              
+<div class="nim-service-detail">
+               
+ <h4>Dedicated Support</h4>
+                
+<p></p>
+              
+</div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+          
+</div> <!-- /col -->         
+        
+</div> <!-- end row -->
+
+        
+<div class="row">
+          
+<div class="col-sm-4">
+            
+<div class="nim-service margin-bottom">
+             
+ <i class="fa fa-bar-chart-o"></i>
+              
+<div class="nim-service-detail">
+                
+<h4>Truly Multipurpose</h4>
+               
+ <p></p>
+             
+ </div> <!-- /nim-service-detail -->
+           
+ </div> <!-- /nim-service margin-bottom -->
+          
+</div> <!-- /col -->
+
+          
+<div class="col-sm-4">
+            
+<div class="nim-service margin-bottom">
+              
+<i class="fa fa-delicious"></i>
+              
+<div class="nim-service-detail">
+               
+ <h4>Unlimited Colors</h4>
+                
+<p></p>
+              
+</div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+         
+ </div> <!-- /col -->
+
+          
+<div class="col-sm-4">
+           
+ <div class="nim-service margin-bottom">
+              
+<i class="fa fa-pencil-square"></i>
+             
+ <div class="nim-service-detail">
+                
+<h4>Easy to customize</h4>
+                
+<p></p>
+             
+ </div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+          
+</div> <!-- /col -->                      
+       
+ </div> <!-- end row -->
+
+      
+</div>  <!-- container -->
+    
+</section>
+ 
+ 
+ 
+<!-- [/Features]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+<!-- [CONTACT]
+ 
+============================================================================================================================-->
+ 
+<!--sub-form-->
+<section class="sub-form text-center" id="eight">
+  
+<div class="container">
+    <div class="col-md-12">
+        
+<h3 class="title">Contact to our <span class="themecolor"> mail ID for any query.</span></h3>
+            
+<p class="lead">Brought to you by GLA University </p>
+    
+</div> 
+    
+<div class="row">
+        
+<div class="col-md-3 col-sm-3"></div>
+      
+<div class="col-md-6 center-block col-sm-6 ">
+        
+<form id="mc-form">
+          
+<div class="input-group">
+            
+<input type="email" class="form-control" placeholder="Email Address" required id="mc-email">
+           
+<span class="input-group-btn">
+            
+<button type="submit" class="btn btn-default"> <i class="fa fa-envelope"></i> </button>
+            
+</span> </div>
+          
+<label for="mc-email" id="mc-notification"></label>
+       
+ </form>
+      
+</div>
+   
+ </div>
+  
+</div>
+
+</section>
+
+<!--sub-form end--> 
+
+
+ 
+ 
+<!-- [/CONTACT]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+<!-- [FOOTER]
+ 
+============================================================================================================================-->
+ 
+
+<footer class="site-footer section-spacing text-center " id="eight">
+    
+  
+<div class="container">
+    
+<div class="row">
+      
+<div class="col-md-4">
+        
+<p class="footer-links"><a href="#"></a> <a href="#"></a></p>
+      
+</div>
+      
+<div class="col-md-4"> <small>&copy; 2019, Brought To You By <a href="https://code-projects.org/">BookRail</a> </small></div>
+      
+<div class="col-md-4"> 
+        
+<!--social-->
+        
+        
+<ul class="social">
+          
+<li><a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter "></i></a></li>
+          
+<li><a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+          
+<li><a href="https://www.youtube.com/" target="_blank"><i class="fa fa-youtube-play"></i></a></li>
+        
+</ul>
+        
+        
+<!--social end--> 
+        
+      
+</div>
+    
+</div>
+  
+</div>
+
+</footer>
+
+ 
+ 
+ 
+<!-- [/FOOTER]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+
+</div>
+ 
+
+<!-- [ /WRAPPER ]
+
+=============================================================================================================================-->
+
+	
+<!-- [ DEFAULT SCRIPT ] -->
+	
+<script src="library/modernizr.custom.97074.js"></script>
+	
+<script src="library/jquery-1.11.3.min.js"></script>
+        
+<script src="library/bootstrap/js/bootstrap.js"></script>
+	
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>	
+	
+<!-- [ PLUGIN SCRIPT ] -->
+        
+<script src="library/vegas/vegas.min.js"></script>
+	
+<script src="js/plugins.js"></script>
+        
+<!-- [ TYPING SCRIPT ] -->
+         
+<script src="js/typed.js"></script>
+         
+<!-- [ COUNT SCRIPT ] -->
+         
+<script src="js/fappear.js"></script>
+       
+<script src="js/jquery.countTo.js"></script>
+	
+<!-- [ SLIDER SCRIPT ] -->  
+        
+<script src="js/owl.carousel.js"></script>
+         
+<script src="js/jquery.magnific-popup.min.js" type="text/javascript"></script>
+        
+<script type="text/javascript" src="js/SmoothScroll.js"></script>
+        
+        
+<!-- [ COMMON SCRIPT ] -->
+	<script src="js/common.js"></script>
+  
+</body>
+
+
+</html>
